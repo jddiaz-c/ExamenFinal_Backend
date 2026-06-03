09bf58e1bbaef8706e4fa9e8e38c12bf14e1bd9c
jddiaz-c <jddiazc@uniboyaca.edu.co>
2026-06-02 23:41:54 -0500

    jddiazc: [Archivo index.php de ms_incapacidades]

---
+++ ms_incapacidades/public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use MsCore\Middlewares\CorsMiddleware;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+require __DIR__ . '/../app/Config/Database.php';
+
+$routes = require __DIR__ . '/../app/Incapacidades/Routes/endpoints.php';
+
+$app = AppFactory::create();
+
+$app->options('/{routes:.+}', fn($req, $res) => $res);
+$app->add(new CorsMiddleware());
+
+$routes($app);
+
+$app->run();
